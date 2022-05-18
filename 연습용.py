9f8795384cd2726bce7f51c7b74f8a0daf0adb9a
Josungyeon <zounsam@naver.com>
2022-05-18 21:50:59 +0900

    baekjoon 공부

--- 연습용.py
+++ 연습용.py
@@ -1,10 +1,14 @@
-a,b,c=map(int,(input().split())
-
+a=list(map(int,(input().split())))
+re = 0
 if a[0]==a[1] & a[0]==a[2] & a[1]==a[2] :
     re=10000
     print(re+a[0]*1000)
-elif a[0]!=a[1] & a[0]!=a[2] & a[1]!=a[2]:
-    print(max(a)*100)
+elif a[0]==a[1] & a[0]==a[2] & a[1]==a[2]:
+    max=0
+    for i in a :
+        if max<i:
+            max = i
+    print(int(max)*100)
 else:
     re=1000
     if a[0]==a[1]:
@@ -13,3 +17,5 @@ else:
         print(re + a[0] * 100)
     elif a[1]==a[2]:
         print(re+a[1]*100)
+
+
