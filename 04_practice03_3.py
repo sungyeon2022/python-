f0765532e868025087e850ea44b4f5a552243b17
Josungyeon <zounsam@naver.com>
2022-05-18 20:50:31 +0900

    수업

---
+++ 04_practice03_3.py
@@ -0,0 +1,3 @@
+a={'flower':'꽃','fly':'날다','floor':'바닥','kick':'차다'}
+b=input('영어 단어를 입력하세요 >>> ')
+print(f'{b} : {a[b]}')
--- 연습용.py
+++ 연습용.py
@@ -1,2 +1,16 @@
-a,b = map(int,input().split())
-print(a*(b-1)+1)
+t = int(input())
+res = 0
+for i in range(t):
+    res = 0
+    sol = list(input().split())
+    for j in sol:
+        if j == sol[0]:
+            res += float(j)
+        else:
+            if j == '@':
+                res *= 3
+            elif j == '%':
+                res += 5
+            elif j == '#':
+                res -= 7
+    print('%.2f' %res)
