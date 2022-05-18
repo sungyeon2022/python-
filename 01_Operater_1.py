6142cae977d12688216c8266c206bc4ecdcad247
Josungyeon <zounsam@naver.com>
2022-05-18 21:45:31 +0900

    baekjoon 공부

---
+++ 01_Operater_1.py
@@ -0,0 +1,18 @@
+a=10
+b=7
+'''
+    산술연산자
+        + 덧셈, - 뺄셈, * 곱셈, /나눗셈(소수점), //(몫만), %(나머지), **(제곱)
+    비교연산자
+        
+'''
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a==b)
+print(a!=b)
+print(a?b)
+
