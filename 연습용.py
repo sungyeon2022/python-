d22dfb3d6b2d9016424f3ac6ddc4e8f1bb5060cf
Josungyeon <zounsam@naver.com>
2022-05-23 20:35:34 +0900

    연산자 공부

--- 연습용.py
+++ 연습용.py
@@ -1,21 +1,8 @@
-a=list(map(int,(input().split())))
-re = 0
-if a[0]==a[1] & a[0]==a[2] & a[1]==a[2] :
-    re=10000
-    print(re+a[0]*1000)
-elif a[0]==a[1] & a[0]==a[2] & a[1]==a[2]:
-    max=0
-    for i in a :
-        if max<i:
-            max = i
-    print(int(max)*100)
-else:
-    re=1000
-    if a[0]==a[1]:
-        print(re+a[0]*100)
-    elif a[0]==a[2]:
-        print(re + a[0] * 100)
-    elif a[1]==a[2]:
-        print(re+a[1]*100)
-
-
+while 1==1 :
+    t = list(map(int, input().split()))
+    if t[0]>t[1] :
+        print('Yes')
+    elif t[0]==0 and t[1]==0 :
+        break
+    else:
+        print('No')
