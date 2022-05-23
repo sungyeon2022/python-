a6c746b11074a837dad02f9ffcff864de5e9155e
Josungyeon <zounsam@naver.com>
2022-05-23 21:38:25 +0900

    연산자 공부

--- 연습용.py
+++ 연습용.py
@@ -1,8 +1,3 @@
-while 1==1 :
-    t = list(map(int, input().split()))
-    if t[0]>t[1] :
-        print('Yes')
-    elif t[0]==0 and t[1]==0 :
-        break
-    else:
-        print('No')
+a= list(map(int,input(). split()))
+result = a[0]*a[1]-a[2]
+print(result if result>0 else 0)
