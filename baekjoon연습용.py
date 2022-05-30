7bad6e836bd7d429b0fbba2d080e556b3c433aca
Josungyeon <zounsam@naver.com>
2022-05-30 21:49:14 +0900

    for문의 컨트롤

--- baekjoon연습용.py
+++ baekjoon연습용.py
@@ -1,11 +1,10 @@
 max=list()
-mi =0
 min=list()
 a= int(input())
 for i in range(a+1):
     mi=int(input())
     if len(max)==0 and len(min)==0:
-        print(mi)
-        max.append(mi)
+        min.append(mi)
     else:
-        if max[-1]
+
+    print(min[-1])
